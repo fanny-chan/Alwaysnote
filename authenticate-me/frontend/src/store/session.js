ef11f61a24debc26de16d2357a189c713632d34e
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-09-10 15:16:44 -0400

    Session Actions and Reducer

---
+++ authenticate-me/frontend/src/store/session.js
@@ -0,0 +1,53 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+// POJO action 1: set the session user in the session slice of state
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+// POJO action 2: remove the session user
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+// login thunk action
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
