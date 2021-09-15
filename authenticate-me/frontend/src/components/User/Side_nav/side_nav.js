082582f0a8edcea8872c7cab118e58664cb89498
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-09-15 12:34:40 -0400

    got notebooks on nav

--- authenticate-me/frontend/src/components/User/Side_nav/side_nav.js
+++ authenticate-me/frontend/src/components/User/Side_nav/side_nav.js
@@ -2,8 +2,21 @@
 // import './User.css';
 // import GetNotebookForm from "../NotebookFormPage/GetNotebookForm";
 
-// const UserSideNav =() => {
+// const UserMainNav =() => {
 //     return (
-
+        
+//             <div class="side-nav"></div>
+//                 <div className="user-email">
+//                 <div className="new-notebook"></div>
+//                 <div className="get-notebook-form" style={{color:"pink"}}>
+//                     <GetNotebookForm />
+//                 </div>
+//                 <div className="update-notebook-form">
+//                     <UpdateNotebookForm />
+//                 </div>
+                
+                    
+//                 </div>
+//         </div>
 //     )
 // }
--- authenticate-me/frontend/src/components/User/index.js
+++ authenticate-me/frontend/src/components/User/index.js
@@ -5,16 +5,21 @@ import UpdateNotebookForm from "../NotebookFormPage/UpdateNotebookForm";
 
 
 const UserMain =() => {
+
     return (
         <div class="main-container">
-            <div class="side-nav"></div>
+            <div class="side-nav">
                 <div className="user-email">
-                <div className="notebook-form">
+                <div className="new-notebook"></div>
+                <div className="get-notebook-form" style={{color:"pink"}}>
                     <GetNotebookForm />
+                </div>
+                <div className="update-notebook-form">
                     <UpdateNotebookForm />
                 </div>
-                    
-                </div>
+            
+                </div>            
+            </div>
         </div>
     )
 }
