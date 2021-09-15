67e19432abc620767dda11a333bb771370283e2e
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-09-15 12:08:39 -0400

    css fix

---
+++ authenticate-me/frontend/src/components/User/Side_nav/side_nav.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import './User.css';
+// import GetNotebookForm from "../NotebookFormPage/GetNotebookForm";
+
+// const UserSideNav =() => {
+//     return (
+
+//     )
+// }
