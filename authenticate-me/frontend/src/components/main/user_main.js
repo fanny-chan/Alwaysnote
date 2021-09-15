1d5e80a85a363637a3d5248669598d22c6a127be
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-09-14 23:13:10 -0400

    created user css page

--- authenticate-me/frontend/src/components/main/user_main.js
+++ authenticate-me/frontend/src/components/main/user_main.js
@@ -3,7 +3,12 @@ import React from "react";
 
 const UserMain =() => {
     return (
-        <div></div>
+        <div class="main-container">
+            <div class="side-nav"></div>
+                <div className="user-email">
+                    
+                </div>
+        </div>
     )
 }
 export default UserMain;
